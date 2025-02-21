644dfde8159c0edbdd925808f08e8ee70cbcc188
Vicente118 <vdarras27@gmail.com>
2025-02-21 13:25:29 +0100

    libasm without bonus OK

--- libasm/main.c
+++ libasm/main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <errno.h>
 
+
+ssize_t ft__write(int fd, const void *buf, size_t count);
+ssize_t ft__read(int fd, void *buf, size_t count);
 int     ft__strlen(const char *str);
 int     ft__strcmp(const char *s1, const char *s2);
 char    *ft__strcpy(const char *source, const char *destination);
+char    *ft__strdup(const char *s);
 
 int main()
 {
@@ -29,8 +36,38 @@ int main()
     printf("ft__strcmp(s1, s3) = %d\n", ft__strcmp(s1, s3));
     printf("ft__strcmp(s1, s1) = %d\n\n", ft__strcmp(s1, s1));
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
+    ssize_t res = ft__write(1, "ft__write() is writing on stdout !\n", 36);
+    if (res == -1)
+        perror("Error: ");
+
+    res = ft__write(-1, "ft__write() is writing on stdout !\n", 36);
+    if (res == -1)
+        perror("Error: ");
+    ft__write(1, "\n\n", 2);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+    char *buffer = malloc(1024);
+    memset(buffer, 0, 1024);
+
+    res = ft__read(1, buffer, 1024);
+    if (res == -1)
+        perror("Error: ");
+    printf("%s", buffer);
+
+    res = ft__read(-1, buffer, 1024);
+    if (res == -1)
+        perror("Error: ");
+    printf("\n\n");
+    free(buffer);
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+    char *dup = ft__strdup("New string from ft__strdup !");
+
+    printf("%s\n", dup);
+    free(dup);
 
     return 0;
 }
